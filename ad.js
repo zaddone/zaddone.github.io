3393ee71f05684c7b120a7a846888ced6848a754
EC2 Dimon <dimon@zaddone.com>
2022-02-07 11:31:38 +0000

    bak

--- ad.js
+++ ad.js
@@ -1,4 +1,13 @@
-document.write("<script src='https://glimtors.net/pfe/current/tag.min.js?z=4851842' data-cfasync='false' async></script>");
-document.write("<script src='https://stootsou.net/pfe/current/tag.min.js?z=4851842' data-cfasync='false' async></script>");
-document.write("<script>(function(d,z,s){s.src='//'+d+'/400/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('rndskittytor.com',4851913,document.createElement('script'))</script>")
+function addJsSrc(url){
+	var script = document.createElement('script');
+    script.type = 'text/javaScript';
+	script.setAttribute('data-cfasync','false');
+	script.async = true;
+    script.src = url;
+    document.getElementsByTagName('head')[0].appendChild(script);
+}
+addJsSrc('https://glimtors.net/pfe/current/tag.min.js?z=4851842')
+addJsSrc('https://stootsou.net/pfe/current/tag.min.js?z=4851842')
+(function(d,z,s){s.src='//'+d+'/400/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('rndskittytor.com',4851913,document.createElement('script'))
+
 
