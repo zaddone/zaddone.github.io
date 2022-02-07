d606ae4dca6d123cdc17682d631f5e36baae1009
EC2 Dimon <dimon@zaddone.com>
2022-02-07 12:00:20 +0000

    bak

--- ad.js
+++ ad.js
@@ -1,13 +1,9 @@
-function addJsSrc(url){
-	var script = document.createElement('script');
-    script.type = 'text/javaScript';
-	script.setAttribute('data-cfasync','false');
-	script.async = true;
-    script.src = url;
-    document.getElementsByTagName('head')[0].appendChild(script);
-}
-addJsSrc('https://glimtors.net/pfe/current/tag.min.js?z=4851842')
-addJsSrc('https://stootsou.net/pfe/current/tag.min.js?z=4851842')
-(function(d,z,s){s.src='//'+d+'/400/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('rndskittytor.com',4851913,document.createElement('script'))
-
+atOptions = {
+		'key' : '2b4e14c7c1a6275110219d4253dce7e9',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplaycontent.com/2b4e14c7c1a6275110219d4253dce7e9/invoke.js"></scr' + 'ipt>');
 
