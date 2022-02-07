1a6a9ca3586ebf4a530afa013cc468669b0b9d4f
EC2 Dimon <dimon@zaddone.com>
2022-02-07 11:08:25 +0000

    bak

--- ad.js
+++ ad.js
@@ -1,3 +1,4 @@
 document.write("<script src='https://glimtors.net/pfe/current/tag.min.js?z=4851842' data-cfasync='false' async></script>");
 document.write("<script src='https://stootsou.net/pfe/current/tag.min.js?z=4851842' data-cfasync='false' async></script>");
+document.write("<script>(function(d,z,s){s.src='//'+d+'/400/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('rndskittytor.com',4851913,document.createElement('script'))</script>")
 
