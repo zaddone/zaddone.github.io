067641f9f1610c55205a781e25b96f0433830081
EC2 Dimon <dimon@zaddone.com>
2022-02-07 05:36:28 +0000

    bak

--- search.js
+++ search.js
@@ -1,33 +1,56 @@
-	function getUrlParam(name) {
-		var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
-		var r = window.location.search.substr(1).match(reg); //匹配目标参数
-		if (r != null) return decodeURIComponent(r[2]);
-		return null;
-	}
-	function dataToHtml(db){
-		var h='<div class="bin col-6 col-md-2"><a class="card link-secondary text-decoration-none" href="/'+db.Dir+'/'+db.Name+'.html"><img class="card-img-top" src="'+db.Img+'" alt="'+db.Name+'">'
-		h+='<div class="card-body">'
-		h+='<p>'+db.Name+'</p>'
-		h+='</div></a></div>'
-			return h
-	}
+function getUrlParam(name) {
+	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+	var r = window.location.search.substr(1).match(reg); 
+	if (r != null) return decodeURIComponent(r[2]);
+	return null;
+}
+function dataToHtml(db){
+	var h='<div class="bin col-6 col-md-2"><a class="card link-secondary text-decoration-none" href="/'+db.Dir+'/'+db.Name+'.html"><img class="card-img-top" src="'+db.Img+'" alt="'+db.Name+'">'
+	h+='<div class="card-body">'
+	h+='<p>'+db.Name+'</p>'
+	h+='</div></a></div>'
+	return h
+}
+function dataToHtml_(db){
+	var h='<div class="bin col-6 col-md-2"><a class="card link-secondary text-decoration-none" href="'+db.Dir+'"><img class="card-img-top" src="'+db.Img+'" alt="'+db.Name+'">'
+	h+='<div class="card-body">'
+	h+='<p>'+db.Name+'</p>'
+	h+='</div></a></div>'
+	return h
+}
+function getHistoryList(){
 
-	$(document).ready(function() {
-		let k = getUrlParam('q')
-		if (k) {
-			$(".search_word").val(k)
-			$("title").text(k)
-			$.get("https://zaddone.tk/search/?q=" + encodeURIComponent(k), function(res) {
-				if (!res){
-						return
-				}
-				var h = ''
-				var list = JSON.parse(res)
-				list.forEach(function(v){
-					h+=dataToHtml(v)			
-				})
-				h+='<hr class="col">'
-				$(".item").prepend(h)
-			})
-		}
+	if (!window.localStorage) return
+	let list_db = localStorage.getItem('list');
+	if (!list_db)return
+	let hdb = ""
+	list_db.split(",").forEach(function(v){
+		let db = localStorage.getItem(v);
+		if (!db)return
+		let h = JSON.parse(db)
+		h.Name = v
+		hdb += dataToHtml_(h)
 	})
+	$(".item").prepend(h)
+
+}
+
+$(document).ready(function() {
+	getHistoryList()
+	let k = getUrlParam('q')
+	if (k) {
+		$(".search_word").val(k)
+		$("title").text(k)
+
+		$.get("https://zaddone.tk/search/?q=" + encodeURIComponent(k), function(res) {
+			if (!res)return
+			var h = ''
+			var list = JSON.parse(res)
+			list.forEach(function(v){
+				h+=dataToHtml(v)			
+			})
+			h+='<hr class="col">'
+			$(".item").prepend(h)
+		})
+	}
+})
--- view.js
+++ view.js
@@ -1,82 +1,91 @@
-function play(url){
-  if (Hls.isSupported()) {
-    var video = document.getElementById('my_video');
-	var hls = new Hls();
-    hls.attachMedia(video);
-    hls.on(Hls.Events.MEDIA_ATTACHED, function () {
-       hls.loadSource(url);
-      hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
-        video.play()
-      });
-    });
-  }else{
-    var video = document.getElementById('my_video');
-	video.pause()
-	video.setAttribute("src",url)
-	video.setAttribute("x5-video-player-fullscreen","true")
-	video.setAttribute("x5-video-player-type","h5")
-	video.setAttribute("x5-playsinline","true")
-	video.setAttribute("webkit-playsinline","true")
-	video.setAttribute("playsinline","true")
-	video.setAttribute("x-webkit-airplay","true")
-	video.controls = true
-	video.load()
-    video.play()
-	
-  }
+function play(url) {
+	if (Hls.isSupported()) {
+		var video = document.getElementById('my_video');
+		var hls = new Hls();
+		hls.attachMedia(video);
+		hls.on(Hls.Events.MEDIA_ATTACHED, function() {
+			hls.loadSource(url);
+			hls.on(Hls.Events.MANIFEST_PARSED, function(event, data) {
+				video.play()
+			});
+		});
+	} else {
+		var video = document.getElementById('my_video');
+		video.pause()
+		video.setAttribute("src", url)
+		video.setAttribute("x5-video-player-fullscreen", "true")
+		video.setAttribute("x5-video-player-type", "h5")
+		video.setAttribute("x5-playsinline", "true")
+		video.setAttribute("webkit-playsinline", "true")
+		video.setAttribute("playsinline", "true")
+		video.setAttribute("x-webkit-airplay", "true")
+		video.controls = true
+		video.load()
+		video.play()
+
+	}
 }
-function showfirst(){
- $('#nav-tab button').first().click()
- $('.video-btn').first().click()
+
+function showfirst() {
+	$('#nav-tab button').first().click()
+	$('.video-btn').first().click()
 }
-$(document).ready(function(){
-   $(".video-btn").click(function(){
-        $(".btn-dark").removeClass("btn-dark")
-     $(this).addClass("btn-dark")
-     $(".video-title > span").html($(this).html())
-     vinfo={url:$(this).attr('video-url'),name:$(this).parent().attr("id")}
-       //console.log(vinfo.name)
-     play(vinfo.url)
-     let title= $('title').text().split("_")[0]
-     if(window.localStorage){
-		localStorage.setItem(title,JSON.stringify(vinfo))
-       let list_db = localStorage.getItem('list');
-       if (list_db  ){
-         if (list_db.indexOf(title)<0){
-           let list =  list_db.split(',')
-           list.push(title)
-           if (list.length>30)
-             localStorage.removeItem(list.shift())
-
-           list_db= list.join(',')
-            localStorage.setItem('list',title)
-         }
-       }else{
-         localStorage.setItem('list',title)
-
-       }
-     }
-	 $.get("https://zaddone.tk/set_hot" , {name:title,img:$("video").attr("poster")})
-
-  })
-  if(window.localStorage){
-    let vinfo = localStorage.getItem($('.video-title').text());
-    if (!vinfo){
-      showfirst()
-      return
-    };
-    console.log(vinfo)
-    let h = JSON.parse(vinfo)
-   $(".nav-link").removeClass("active")
-   $("#"+h.name+"-tab").addClass("active")
-   $(".tab-pane").removeClass("active show")
-   $("#"+h.name).addClass("active show")
-   $(".video-btn").each(function(){
-     if ($(this).attr("video-url")==h.url){
-       $(this).click()
-     }
-   })
+function addHistoryList(title){
+	let list_db = localStorage.getItem('list');
+	if (!list_db){
+    	localStorage.setItem('list',title);
     }else{
-      showfirst()
+        var li = list_db.split(",")
+        li.push(title)
+		li = Array.from(new Set(li))
+		if (li.length>30){
+			localStorage.removeItem(li.shift())
+		}
+    	localStorage.setItem('list',li.join(","))
     }
+}
+$(document).ready(function() {
+	$(".video-btn").click(function() {
+		$(".btn-dark").removeClass("btn-dark")
+		$(this).addClass("btn-dark")
+		$(".video-title > span").html($(this).html())
+		var img = $("video").attr("poster")
+		vinfo = {
+			url: $(this).attr('video-url'),
+			tag: $(this).parent().attr("id"),
+			Img:img,
+			Dir:window.location,
+		}
+		//console.log(vinfo.name)
+		play(vinfo.url)
+		let title = $('title').text().split("_")[0]
+		$.get("https://zaddone.tk/set_hot", {
+			name: title,
+			img: img,
+		})
+		if (!window.localStorage)return
+		localStorage.setItem(title, JSON.stringify(vinfo))
+		addHistoryList(title)
+	})
+	if (window.localStorage) {
+		let title = $('title').text().split("_")[0]
+		let vinfo = localStorage.getItem(title);
+		if (!vinfo) {
+			showfirst()
+			return
+		};
+		console.log(vinfo)
+		let h = JSON.parse(vinfo)
+		$(".nav-link").removeClass("active")
+		$("#" + h.tag + "-tab").addClass("active")
+		$(".tab-pane").removeClass("active show")
+		$("#" + h.tag).addClass("active show")
+		$(".video-btn").each(function() {
+			if ($(this).attr("video-url") == h.url) {
+				$(this).click()
+			}
+		})
+	} else {
+		showfirst()
+	}
 });
