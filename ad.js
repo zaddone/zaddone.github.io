148729a66e1457d2c7168cb6832a431f69f22763
EC2 Dimon <dimon@zaddone.com>
2022-02-10 04:02:34 +0000

    update

--- ad.js
+++ ad.js
@@ -1,15 +1,1 @@
-atOptions = {
-		'key' : '2b4e14c7c1a6275110219d4253dce7e9',
-		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
-		'params' : {}
-	};
-(function() {
-     var s = document.createElement('script');
-     s.type = 'text/javascript';
-     s.async = true;
-     s.src = 'https://www.effectivedisplaycontent.com/2b4e14c7c1a6275110219d4253dce7e9/invoke.js';
-     var x = document.getElementsByTagName('script')[0];
-     x.parentNode.insertBefore(s, x);
- })();
+
